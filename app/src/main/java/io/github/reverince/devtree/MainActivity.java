ec40d7423bec0f970f2de0174e5b7b619d4f7542
YJ CHO <reverince@gmail.com>
2019-11-30 23:05:58 +0900

    Add bottom navbar

--- app/src/main/java/io/github/reverince/devtree/MainActivity.java
+++ app/src/main/java/io/github/reverince/devtree/MainActivity.java
@@ -1,20 +1,41 @@
 package io.github.reverince.devtree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, MyBoardActivity.class);
-        startActivity(intent);
-        finish();
+        bottomNavigationView = findViewById(R.id.nav_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_board:
+                        Intent intent = new Intent(MainActivity.this, MyBoardActivity.class);
+                        startActivity(intent);
+                        //finish();
+                        break;
+                    case R.id.action_home:
+                        break;
+                    case R.id.action_profile:
+                        //Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/io/github/reverince/devtree/MyBoardActivity.java
+++ app/src/main/java/io/github/reverince/devtree/MyBoardActivity.java
@@ -1,19 +1,25 @@
 package io.github.reverince.devtree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MyBoardActivity extends AppCompatActivity {
     final String[] titles = { "과제 01", "과제 02", "과제 03" };
     final String[] contents = { "01 ", "02 ", "03 " };
 
+    BottomNavigationView bottomNavigationView;
     GridView boardGrid;
     TextView taskTitleText, taskContentText;
 
@@ -30,6 +36,26 @@ public class MyBoardActivity extends AppCompatActivity {
 
         taskTitleText = findViewById(R.id.text_task_title);
         taskContentText = findViewById(R.id.text_task_content);
+
+        bottomNavigationView = findViewById(R.id.nav_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_board:
+                        break;
+                    case R.id.action_home:
+                        Intent intent = new Intent(MyBoardActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case R.id.action_profile:
+                        //Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private AdapterView.OnItemClickListener onCellClickListener = new AdapterView.OnItemClickListener() {
